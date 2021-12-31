489ce277b1430bb0be57ebd7821b61e224674394
kntowd <kenta2231989@gmail.com>
2021-12-31 14:50:57 +0900

    titleとアイコンの変更

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'src',
+    title: 'pokery',
     htmlAttrs: {
       lang: 'en'
     },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
     ]
   },
 
