aa2b1f34a20c70356834d774b3697c184d78678e
kntowd <kenta2231989@gmail.com>
2021-12-31 18:15:47 +0900

    api serverを呼び出せるようにした

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,5 +7,9 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "IndexPage",
+
+  fetch() {
+    fetch('http://localhost:8080').then(response => response.json()).then(data => console.log(data))
+  },
 });
 </script>
