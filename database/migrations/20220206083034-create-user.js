3388b78ac8726f94c078062bc5d596f90244aba0
kntowd <kenta2231989@gmail.com>
2022-02-06 18:28:25 +0900

    users tableを追加するmigration file を追加

---
+++ database/migrations/20220206083034-create-user.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      point: {
+        type: Sequelize.INTEGER,
+      },
+      room_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "rooms",
+            key: "id",
+          },
+        },
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
