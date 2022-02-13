b56f5941ba54c0f2334076309e6f87057dabf847
kntowd <kenta2231989@gmail.com>
2022-02-13 11:22:34 +0900

    table構造を変更

--- database/migrations/20220110034149-create-rooms.js
+++ database/migrations/20220110034149-create-rooms.js
@@ -2,13 +2,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable("rooms", {
       id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      roomId: {
         unique: true,
+        primarykey: true,
         type: Sequelize.INTEGER,
       },
       createdAt: {
