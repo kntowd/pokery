3f3f3f937d0c3e8099526bb49df6ba525a2b72ca
kntowd <kenta2231989@gmail.com>
2022-02-27 18:28:46 +0900

    部屋に紐づくユーザがいないときリダイレクト

--- src/pages/rooms/_roomId/index.vue
+++ src/pages/rooms/_roomId/index.vue
@@ -19,13 +19,22 @@ export default class Room extends Vue {
 
   socket = null;
 
-  created() {
+  async created() {
+    const { apiBaseUrl } = this.$nuxt.context.$config.env;
     if (sessionStorage.userId == null || sessionStorage.roomId == null) {
-      console.log("redirect");
       this.$router.push(`/rooms/${this.$route.params.roomId}/users`);
     }
 
-    const { apiBaseUrl } = this.$nuxt.context.$config.env;
+    const response = await fetch(
+      `${apiBaseUrl}/api/users/${sessionStorage.userId}/${this.$route.params.roomId}`
+    );
+
+    const user = await response.json();
+
+    if (user.length === 0) {
+      this.$router.push(`/rooms/${this.$route.params.roomId}/users`);
+    }
+
     this.socket = io(apiBaseUrl);
     this.socket.emit("join_room", { roomId: this.$route.params.roomId });
     this.socket.on("joined_room", (data) => console.log(data));
