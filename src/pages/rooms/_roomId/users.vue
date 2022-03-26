618d3623ddb7e283066ac1f1f341072ea69a49c4
kntowd <kenta2231989@gmail.com>
2022-03-26 14:02:10 +0900

    ルーティング処理を書き換え

--- src/pages/rooms/_roomId/users.vue
+++ src/pages/rooms/_roomId/users.vue
@@ -16,14 +16,9 @@ export default class CreateUser extends Vue {
   userId = null;
 
   async created() {
-    const { apiBaseUrl } = this.$nuxt.context.$config.env;
     const { roomId } = this.$route.params;
 
-    const response = await fetch(
-      `${apiBaseUrl}/api/users/${localStorage.userId}/${roomId}`
-    );
-
-    const user = await response.json();
+    const user = await this.$users.get(this.userId, roomId);
 
     if (user.length !== 0) {
       this.$router.push(`/rooms/${roomId}`);
@@ -31,7 +26,6 @@ export default class CreateUser extends Vue {
   }
 
   createUser() {
-    const { appBaseUrl } = this.$nuxt.context.$config.env;
     const { roomId } = this.$route.params;
 
     localStorage.setItem("roomId", roomId);
@@ -39,7 +33,8 @@ export default class CreateUser extends Vue {
     const { userId } = this.$users.post(roomId, this.name);
 
     localStorage.setItem("userId", userId);
-    window.location.href = `${appBaseUrl}/rooms/${roomId}`;
+
+    this.router.push(`/rooms/${roomId}`);
   }
 }
 </script>
